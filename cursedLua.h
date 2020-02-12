d79411288a031c9d77d2ddcb8d7547ff1cfbf9c5
Arkaeriit <maxbouillot@gmail.com>
2020-02-12 14:22:21 +0100

    General improvement

--- cursedLua.h
+++ cursedLua.h
@@ -8,6 +8,7 @@
 #include <lualib.h>
 #include <lauxlib.h>
 #include <locale.h>
+#include <unistd.h>
 
 //Foncions qui servent à un crontrole général de l'écrant
 
@@ -19,13 +20,17 @@ int cl_cursset(lua_State *L); //permet de chisir l'état du curseur
 int cl_refresh(lua_State *L); //permet de réactualiser l'écrant
 int cl_getxy(lua_State *L); //Permet de savoir les dimentions de l'écrant
 int cl_getch(lua_State *L); //Permet de récupérer les inputs de l'utilisateur
+int cl_getchTime(lua_State* L); //Permet de récupérer un input avent un timout (en µs) avec un découpage temporel (en µs)
 int cl_hascolor(lua_State *L);
 int cl_startcolor(lua_State *L);
+int cl_defaultcolors(lua_State *L); //permet d'avoir use_deffault_colors
+int cl_init_pair(lua_State *L); //Permet d'initialiser une paie de couleur.
 
 //Fonctions servant à écrire sur l'écrant
 
 int cl_mvprintw(lua_State *L); //permet d'écrire sur l'écrant
 int cl_color(lua_State *L); //S'occupe du choix des couleurs, ne marche pas dirrectement comme dans ncurses mais est plus direct, on l'utilise avec deux arguments en entrée pour le choix des couleurs et les couleurs sont choisies.
+int cl_set_color(lua_State *L); //Permet de choisir sur l'écran une paire de couleur.
 
 
 //fonction à appeler en C pour charger la librairie
--- exemple.lua
+++ exemple.lua
@@ -2,6 +2,7 @@
 function main()
     initscr()
     curs_set(0)
+    start_color()
     local y,x = getmaxyx()
     x = (x - x%2)/2 - 7
     y = (y - y%2)/2 
